dbbbd788d0603f87d8ed1f7d170256138f51f18b
MaxwellYJiang <maxwell.jiang1@gmail.com>
2024-11-24 23:31:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # horizontal velocity 10 m/s
 # boundary conditions are based on what is the surface
 # Do we need to assume that the surface top has the same velocities as if we hit the surface
+# or could run multiple iterations to solve the missing points.
 dz = 5
 dx = 5
 # Surface Funciton 10sin(pi x/25)
